69ffe6491c97095dee8e309d6ee655c44ea59622
Krzysztof <krzysztof.michalik01@gmail.com>
2020-05-06 16:24:59 +0200

    Added saving post mechanism. (save post  & checker if user is logged )

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,20 +3,23 @@
 @section('content')
 <div class="container">
   <form action="/p" enctype="multipart/form-data" method="post">
+
+    @csrf
+
     <div class="row">
       <div class="col-8 offset-2">
         <div class="row">
           <h1>Add New Post</h1>
         </div>
+
         <div class="form-group row">
-          <label for="name" class="col-form-label">Post Caption</label>
+          <label for="caption" class="col-form-label">Post Caption</label>
             <input  id="caption"
               type="text"
               class="form-control
-              @error('caption') is-invalid @enderror"
+              @error('caption') is-invalid @enderror "
               name="caption"
               value="{{ old('caption') }}"
-              required
               autocomplete="caption" autofocus>
 
               @error('caption')
@@ -24,21 +27,20 @@
                 <strong>{{ $message }}</strong>
               </span>
               @enderror
-
           </div>
 
           <div class="row">
             <label for="image" class="col-form-label">Post Image</label>
             <input type="file" class="form-control-file" id="image" name="image">
             @error('image')
-            <span class="invalid-feedback" role="alert">
+            
               <strong>{{ $message }}</strong>
-            </span>
+            
             @enderror
           </div>
 
           <div class="row pt-4">
-            <button type="button" class='btn btn-primary' name="button">Add New Post</button>
+            <button type="submit" class='btn btn-primary' name="button">Add New Post</button>
           </div>
         </div>
       </div>
